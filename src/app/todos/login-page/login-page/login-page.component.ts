044cbbefd2626d136cebca463c4f57237c00cb6e
abdullahbeker <abdullah.beker@pia-team.com>
2023-06-28 01:57:44 +0300

    create login component

---
+++ src/app/todos/login-page/login-page/login-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+  sendUserInformation(data:any){
+    console.log(data)
+  }
+}
